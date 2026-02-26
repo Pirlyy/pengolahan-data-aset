bbf95c452893c32bee660dc735de41b3e1273630
Pirlyy <wfirlyari@gmail.com>
2026-02-26 16:17:47 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+ use Illuminate\Support\Facades\DB;
+
+Route::get('/test-mongo', function () {
+    DB::connection('mongodb')->table('test')->insert([
+        'nama' => 'Firly',
+        'created_at' => now()
+    ]);
+
+    return "Data berhasil masuk MongoDB!";
+});
