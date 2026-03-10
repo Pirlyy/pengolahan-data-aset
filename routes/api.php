17dd231c2b0a1a9f7f2fe973770f4d6014c57cf1
areljagad <arelanakganteng@gmail.com>
2026-03-10 14:19:19 +0700

    p

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login',    [AuthController::class, 'login']);
+});
+
+//new
+Route::middleware('auth:api')->prefix('auth')->group(function () {
+    Route::post('/logout',  [AuthController::class, 'logout']);
+    Route::get('/me',       [AuthController::class, 'me']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+});
